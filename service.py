d289d0dad507a6c828be91bd5be7bb22dc8b49c9
Near Lg <nearlg@gmail.com>
2018-04-29 17:27:55 +0200

    Fix: improve the kodi service file

--- service.py
+++ service.py
@@ -1,63 +1,40 @@
 """
 Main program
 """
-import socket
-import threading
 import sys
-import xbmc
 import os
+import xbmc
 import xbmcaddon
-sys.path.append(os.path.dirname(os.path.realpath(__file__)) + "/resources/lib")
-from receiver import ReceiverThread
-from supervisor import SupervisorThread
-from connection import Connection
+from resources.gcrserver.main import Main
+
 
 addon = xbmcaddon.Addon()
-PORT = int(addon.getSetting('PORT'))
-GPIO_PORT = int(addon.getSetting('GPIO_PORT'))
-GPIO_PATH = addon.getSetting('GPIO_PATH')
-GPIO_FILE_NAME = os.path.join(GPIO_PATH, 'gpio' + str(GPIO_PORT), 'value')
+addonPath = addon.getAddonInfo("path")
+databasePath = addonPath + addon.getSetting('SOCKET_PORT')
 
-def set_up_gpio(gpio_port):
-    """
-    Prepare the gpio port to be used
-    """
-    addonPath = addon.getAddonInfo("path")
-    scriptPath = os.path.join(addonPath, 'resources', 'lib', 'relay.sh')
-    os.system("sh " + scriptPath + " " + str(gpio_port))
-    #EXPORT_FILE = '/sys/class/gpio/export'
-    #DIRECTION_FILE = '/sys/class/gpio/gpio'+str(gpio_port)+'/direction'
+SOCKET_PORT = int(addon.getSetting('SOCKET_PORT'))
+DB_FILE = databasePath
 
-    #with open(EXPORT_FILE, 'w+') as f:
-    #    f.write(str(gpio_port))
-    #with open(DIRECTION_FILE) as ff:
-    #    ff.write('out')
+
+def main(m):
+    """
+    Main program
+    """
+    monitor = xbmc.Monitor()
+    while not monitor.abortRequested():
+        # Sleep/wait for abort for 10 seconds
+        if monitor.waitForAbort(10):
+            # Abort was requested while waiting. We should exit
+            break
+        m.listen_new_connection()
+
 
 if __name__ == '__main__':
-    monitor = xbmc.Monitor()
-
-    event = threading.Event()
-    ReceiverThread.FILE_NAME = GPIO_FILE_NAME
-    supervisor = SupervisorThread(GPIO_FILE_NAME, event)
-
-    # Create a TCP/IP socket
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    # Bind the socket to the port
-    server_address = ('', PORT)
-    sock.bind(server_address)
-    # Listen for incoming connections
-    sock.listen(1)
-    set_up_gpio(GPIO_PORT)
-    supervisor.start()
-    while not monitor.abortRequested():
-        try:
-            # Wait for a connection
-            conn, client_address = sock.accept()
-            connection = Connection(conn, (client_address[0], PORT), event)
-            connection.start()
-        except:
-            break
-
-    sock.close()
-sys.exit(1)
-
+    m = Main(SOCKET_PORT, DB_FILE)
+    try:
+        main(m)
+    except Exception as e:
+        print('Bye!')
+        xbmc.log('Logging Failure: %s' % (e.message), xbmc.LOGERROR)
+    finally:
+        m.close_socket_connection()
